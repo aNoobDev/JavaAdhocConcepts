59e2aa1210598935a8d410d6247a082d0c79b709
Aftab Alam <alamaftab777@gmail.com>
2023-02-12 14:03:52 +0530

    Java Features

--- Default and Static Methods/src/Cat.java
+++ Default and Static Methods/src/Cat.java
@@ -2,4 +2,7 @@ public interface Cat {
     default void eat(){
         System.out.println("Cat eating");
     }
+    static void meow(){
+        System.out.println("Cat Sound :- Meow Meow");
+    }
 }
--- Default and Static Methods/src/Main.java
+++ Default and Static Methods/src/Main.java
@@ -1,8 +1,12 @@
+/*
+@author=Aftab Alam
+ */
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         Horse horse=new Horse();
         horse.eat();
+        Cat.meow();
     }
 }
